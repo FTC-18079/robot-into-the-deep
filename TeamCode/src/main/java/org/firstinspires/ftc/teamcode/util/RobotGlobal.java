e8bbf937c6cc3cb5e37f298af20b6af18ce90ba8
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-09-07 13:28:12 -0700

    1 project setup (#2)
    * Added FtcDashboard, pedro, and its dependencies
    
    * Added FTCLib dependencies and set up robot classes
    
    * Added utils and template

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotGlobal.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class RobotGlobal {
+    public enum Alliance {
+        RED, BLUE, NONE
+    }
+
+    public static Alliance alliance = Alliance.NONE;
+}
