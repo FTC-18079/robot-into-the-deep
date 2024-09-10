f6f6b11201da8baef8e81253bf0e429a373e48b3
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-09-09 20:18:23 -0700

    11 make psgamepadex (#12)
    * Added touchpad trigger
    
    * Added gamepad rumble

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
@@ -2,13 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.Robot;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RobotCore extends Robot {
     Telemetry telemetry;
+    GamepadEx driveController;
+    GamepadEx manipController;
+    Trigger touchpadTrigger;
 
     public RobotCore() {
+        touchpadTrigger = new Trigger(() -> getTouchpad(driveController));
     }
 
     @Override
@@ -18,5 +24,17 @@ public class RobotCore extends Robot {
 
     public Telemetry getTelemetry() {
         return telemetry;
+    }
+
+    public void rumbleGamepad(GamepadEx gamepad, double rumble1, double rumble2, int ms) {
+        gamepad.gamepad.rumble(rumble1, rumble2, ms);
+    }
+
+    public boolean getTouchpad(GamepadEx gamepad) {
+        return gamepad.gamepad.touchpad;
+    }
+
+    public boolean getPS(GamepadEx gamepad) {
+        return gamepad.gamepad.ps;
     }
 }
