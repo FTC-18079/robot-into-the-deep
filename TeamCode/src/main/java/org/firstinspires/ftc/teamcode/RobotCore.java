ebaf713288aaced6012ce81084d669ba2cf43a67
Nicolas <nicsilvc@gmail.com>
2024-09-09 20:08:57 -0700

    Added touchpad trigger

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
@@ -2,13 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.Robot;
+import com.arcrobotics.ftclib.command.button.Trigger;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RobotCore extends Robot {
     Telemetry telemetry;
+    GamepadEx driveController;
+    GamepadEx manipController;
+    Trigger touchpadTrigger;
 
     public RobotCore() {
+        touchpadTrigger = new Trigger(() -> getTouchpad(driveController));
     }
 
     @Override
@@ -18,5 +24,13 @@ public class RobotCore extends Robot {
 
     public Telemetry getTelemetry() {
         return telemetry;
+    }
+
+    public boolean getTouchpad(GamepadEx gamepad) {
+        return gamepad.gamepad.touchpad;
+    }
+
+    public boolean getPS(GamepadEx gamepad) {
+        return gamepad.gamepad.ps;
     }
 }
