20b1fcb8781df4d7a39362a0ecef282d6943c74f
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-09-11 16:21:03 -0700

    7 tune localizer and follower (#14)
    * Made robotcore
    
    * Added loop time data
    
    * Made teleop
    
    * Fixed teleop
    
    * Fixed driving
    
    * Tuned localizer
    
    * Tuned velocities
    
    * Added sample poses and starting poses
    
    * Tuned acceleration
    
    * Tuned drive, heading, translation pid
    
    * Tuned centripital scaling

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FieldConstants.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+
+// Class for storing poses
+public class FieldConstants {
+    // Robot start poses
+    public enum RobotStart {
+        EXAMPLE
+    }
+
+    public static final Pose EXAMPLE_START_POSE = new Pose();
+
+    // Blue Side Alliance-Specific Element Poses
+    public static final Pose BLUE_LEFT_ALLIANCE_SAMPLE = new Pose(2.5+9.75+10.5, 4*24+1.5);
+    public static final Pose BLUE_MID_ALLIANCE_SAMPLE = new Pose(2.5+9.75, 4*24+1.5);
+    public static final Pose BLUE_RIGHT_ALLIANCE_SAMPLE = new Pose(2.5, 4*24+1.5);
+
+    // Blue Side Neutral Element Poses
+    public static final Pose BLUE_LEFT_NEUTRAL_SAMPLE = new Pose(144-2.5, 4*24+1.5);
+    public static final Pose BLUE_MID_NEUTRAL_SAMPLE = new Pose(144-2.5-9.75, 4*24+1.5);
+    public static final Pose BLUE_RIGHT_NEUTRAL_SAMPLE = new Pose(144-2.5-9.75-10.5, 4*24+1.5);
+
+    // Red Side Alliance-Specific Element Poses
+    public static final Pose RED_LEFT_ALLIANCE_SAMPLE = new Pose(144-2.5-9.75-10.5, 2*24-2.5);
+    public static final Pose RED_MID_ALLIANCE_SAMPLE = new Pose(144-2.5-9.75, 2*24-2.5);
+    public static final Pose RED_RIGHT_ALLIANCE_SAMPLE = new Pose(144-2.5, 2*24-2.5);
+
+    // Red Side Neutral Element Poses
+    public static final Pose RED_LEFT_NEUTRAL_SAMPLE = new Pose(2.5, 2*24-2.5);
+    public static final Pose RED_MID_NEUTRAL_SAMPLE = new Pose(2.5+9.75, 2*24-2.5);
+    public static final Pose RED_RIGHT_NEUTRAL_SAMPLE = new Pose(2.5+9.75+10.5, 2*24-2.5);
+}
