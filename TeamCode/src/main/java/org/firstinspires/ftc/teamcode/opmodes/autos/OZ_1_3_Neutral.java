a1769183dfefe3d7a38e0e6d14c09d9ba1a0801a
Nicolas <nicsilvc@gmail.com>
2024-09-12 16:01:31 -0700

    Added a second path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/OZ_1_3_Neutral.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/OZ_1_3_Neutral.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.autos;
 
 import static org.firstinspires.ftc.teamcode.auto.AutoConstants.*;
 import static org.firstinspires.ftc.teamcode.util.EasyPathBuilder.*;
+import static org.firstinspires.ftc.teamcode.util.RobotGlobal.parkingPose;
 import static org.firstinspires.ftc.teamcode.util.RobotGlobal.robotPose;
 
 import com.arcrobotics.ftclib.command.Command;
@@ -12,21 +13,27 @@ import org.firstinspires.ftc.teamcode.RobotCore;
 import org.firstinspires.ftc.teamcode.auto.AutoTemplate;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 import org.firstinspires.ftc.teamcode.util.FollowPathCommand;
 import org.firstinspires.ftc.teamcode.util.RobotGlobal;
 
 @Autonomous(name = "OZ 1+3 Alliance Samples")
-public class OZ_1_3_Alliance extends AutoTemplate {
+public class OZ_1_3_Neutral extends AutoTemplate {
     // Poses
-    Pose scorePreloadPose = checkAlliance(new Pose(36, 60, Math.toRadians(0)));
+    Pose scorePreloadPose = checkAlliance(new Pose(38, 60, Math.toRadians(0)));
+    Pose scoreBasketPose = checkAlliance(BASKET_SCORE_POSE);
+    Pose parkPosition;
 
     // Paths
     Path scorePreloadPath;
+    Path preloadToBasketPath = linearHeadingPath(scorePreloadPose, scoreBasketPose, scorePreloadPose.getHeading(), scoreBasketPose.getHeading(), 0.8, new Point(new Pose(23, 86)));
 
     @Override
     protected void setStartPose() {
         robotPose = checkAlliance(OBVZONE_STARTING_POSE);
         type = RobotCore.OpModeType.AUTO;
+        parkPosition = (parkingPose == ParkingPose.OBSERVATION_ZONE) ? OBVZONE_PARKING_POSE : SUBMERSIBLE_PARKING_POSE;
+        parkPosition = checkAlliance(parkPosition);
     }
 
     @Override
@@ -37,6 +44,10 @@ public class OZ_1_3_Alliance extends AutoTemplate {
     @Override
     protected Command makeAutoSequence() {
         return new WaitCommand(RobotGlobal.delayMs)
-                .andThen(new FollowPathCommand(robot, scorePreloadPath));
+                .andThen(new FollowPathCommand(robot, scorePreloadPath))
+                //.andThen(scorePreload())
+                //.andThen(collect())
+                .andThen(new FollowPathCommand(robot, preloadToBasketPath));
+                //.andThen(scoreBasket())
     }
 }
