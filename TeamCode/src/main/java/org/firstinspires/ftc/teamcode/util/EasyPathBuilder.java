bbe915c247a7d092d9cdb6a924ed9ce59f9eda12
Nicolas <nicsilvc@gmail.com>
2024-09-12 08:43:09 -0700

    Made tangent and constant heading path builders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/EasyPathBuilder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/EasyPathBuilder.java
@@ -10,21 +10,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class EasyPathBuilder {
-    public static Path makePath(Pose startPose, Pose endPose, double startHeading, double endHeading) {
-        return makePath(startPose, endPose, startHeading, endHeading, 1.0);
+    // Tangent heading paths
+    public Path tangentHeadingPath(Pose startPose, Pose endPose, boolean reverse) {
+        Path path = new Path(new BezierLine(new Point(startPose), new Point(endPose)));
+        path.setTangentHeadingInterpolation();
+        path.setReversed(reverse);
+        return path;
     }
 
-    public static Path makePath(Pose startPose, Pose endPose, double startHeading, double endHeading, double endTime) {
+    public Path tangentHeadingPath(Pose startPose, Pose endPose, boolean reverse, Point... controlPoints) {
+        ArrayList<Point> points = new ArrayList<>();
+
+        points.add(new Point(startPose));
+        points.add(new Point(endPose));
+        points.addAll(Arrays.asList(controlPoints));
+
+        Path path = new Path(new BezierCurve(points));
+        path.setTangentHeadingInterpolation();
+        path.setReversed(reverse);
+        return path;
+    }
+
+    // Constant heading paths
+    public static Path constantHeadingPath(Pose startPose, Pose endPose, double heading) {
+        Path path = new Path(new BezierLine(new Point(startPose), new Point(endPose)));
+        path.setConstantHeadingInterpolation(heading);
+        return path;
+    }
+
+    public static Path constantHeadingPath(Pose startPose, Pose endPose, double heading, Point... controlPoints) {
+        ArrayList<Point> points = new ArrayList<>();
+
+        points.add(new Point(startPose));
+        points.add(new Point(endPose));
+        points.addAll(Arrays.asList(controlPoints));
+
+        Path path = new Path(new BezierCurve(points));
+        path.setConstantHeadingInterpolation(heading);
+        return path;
+    }
+
+    // Linear heading interpolation paths
+    public static Path linearHeadingPath(Pose startPose, Pose endPose, double startHeading, double endHeading) {
+        return linearHeadingPath(startPose, endPose, startHeading, endHeading, 1.0);
+    }
+
+    public static Path linearHeadingPath(Pose startPose, Pose endPose, double startHeading, double endHeading, double endTime) {
         Path path = new Path(new BezierLine(new Point(startPose), new Point(endPose)));
         path.setLinearHeadingInterpolation(startHeading, endHeading, endTime);
         return path;
     }
 
-    public static Path makePath(Pose startPose, Pose endPose, double startHeading, double endHeading, Point... controlPoints) {
-        return makePath(startPose, endPose, startHeading, endHeading, 1.0, controlPoints);
+    public static Path linearHeadingPath(Pose startPose, Pose endPose, double startHeading, double endHeading, Point... controlPoints) {
+        return linearHeadingPath(startPose, endPose, startHeading, endHeading, 1.0, controlPoints);
     }
 
-    public static Path makePath(Pose startPose, Pose endPose, double startHeading, double endHeading, double endTime, Point... controlPoints) {
+    public static Path linearHeadingPath(Pose startPose, Pose endPose, double startHeading, double endHeading, double endTime, Point... controlPoints) {
         ArrayList<Point> points = new ArrayList<>();
 
         points.add(new Point(startPose));
