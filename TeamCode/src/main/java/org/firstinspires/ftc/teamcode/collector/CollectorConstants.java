f6bc3ad5cc17383e079063e403fd5a774b78c11c
Nicolas <nicsilvc@gmail.com>
2024-09-14 16:41:38 -0700

    Fixed hardware test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collector/CollectorConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collector/CollectorConstants.java
@@ -7,7 +7,7 @@ public class CollectorConstants {
     public static double MAX_DELTAV = 200.0;
 
     public static int MAX_SLIDE_POS = 1250;
-    public static int MIN_SLIDE_POS = 100;
+    public static int MIN_SLIDE_POS = 10;
     public static int MIN_VELOCITY = 15;
     public static int ERROR_TOLERANCE = 20;
 
