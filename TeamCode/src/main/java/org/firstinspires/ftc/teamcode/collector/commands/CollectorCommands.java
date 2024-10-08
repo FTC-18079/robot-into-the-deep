ed1cb473f23bd11b9b05f112d6b240fd196077bb
Nicolas <nicsilvc@gmail.com>
2024-10-08 15:51:18 -0700

    Made passthrough command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collector/commands/CollectorCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collector/commands/CollectorCommands.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.collector.commands;
 import com.arcrobotics.ftclib.command.Command;
 
 import org.firstinspires.ftc.teamcode.collector.Collector;
+import org.firstinspires.ftc.teamcode.elevator.Elevator;
 import org.firstinspires.ftc.teamcode.util.commands.Commands;
 
 import java.util.function.Supplier;
@@ -12,9 +13,18 @@ public class CollectorCommands {
 
     static {
         Supplier<Collector> collector = Collector::getInstance;
+        Supplier<Elevator> elevator = Elevator::getInstance;
 
         PASSTHROUGH_COMMAND = () -> Commands.sequence(
-
+                Commands.runOnce(collector.get()::release),
+                Commands.runOnce(elevator.get()::openDoor),
+                Commands.waitMillis(200),
+                Commands.runOnce(elevator.get()::closeDoor)
         );
+    }
+
+    static {
+        Supplier<Collector> collector = Collector::getInstance;
+        Supplier<Elevator> elevator = Elevator::getInstance;
     }
 }
