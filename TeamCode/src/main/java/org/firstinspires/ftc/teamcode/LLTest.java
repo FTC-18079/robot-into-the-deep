0afeed6eea846dc603abe18958eaeb09f762d3f1
Nicolas <nicsilvc@gmail.com>
2024-10-19 00:20:57 -0700

    Sort of works now?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LLTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LLTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -13,17 +14,56 @@ public class LLTest extends OpMode {
     LLVision limelight;
     Servo pivot;
 
+    double pos = 0.5;
+
+    public static double[] HSV_RANGE_YELLOW = {
+            13, 155, 8,    // LOW
+            55, 255, 175    // HIGH
+    };
+
+    public static double[] HSV_RANGE_RED = {
+            13, 155, 8,     // LOW
+            55, 255, 175    // HIGH
+    };
+
+    public static double[] HSV_RANGE_BLUE = {
+            104, 200, 0,    // LOW
+            150, 255, 115   // HIGH
+    };
+
     @Override
     public void init() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        RobotMap.getInstance().init(hardwareMap);
+//        RobotMap.getInstance().init(hardwareMap);
+        RobotMap.getInstance().LIMELIGHT = hardwareMap.get(Limelight3A.class, "limelight");
         limelight = new LLVision(telemetry);
-        pivot = RobotMap.getInstance().PIVOT;
+        limelight.setColorRange(HSV_RANGE_YELLOW);
+        pivot = hardwareMap.get(Servo.class, "pivot");
     }
 
     @Override
     public void loop() {
         limelight.periodic();
-//        pivot.setPosition(limelight.getSampleAngle());
+
+        pos = (limelight.getSampleAngle() / 180.0);
+        pivot.setPosition(pos);
+
+        if (gamepad1.dpad_up) limelight.setPipeline(0);
+        else if (gamepad1.dpad_down) limelight.setPipeline(4);
+
+        if (gamepad1.cross) {
+            limelight.setColorRange(HSV_RANGE_BLUE);
+        } else if (gamepad1.circle) {
+            limelight.setColorRange(HSV_RANGE_RED);
+        } else if (gamepad1.square) {
+            limelight.setColorRange(HSV_RANGE_YELLOW);
+        }
+
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        limelight.stop();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/LLVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/LLVision.java
@@ -2,23 +2,17 @@ package org.firstinspires.ftc.teamcode.vision;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.RobotCore;
 import org.firstinspires.ftc.teamcode.RobotMap;
-import org.firstinspires.ftc.teamcode.collector.CollectorConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LLVision extends SubsystemBase {
     Limelight3A limelight;
     Telemetry telemetry;
     LLResult result;
-    List<LLResultTypes.ColorResult> colorResults;
+    double[] pythonOutput;
+    LLResult lastResult;
 
     private static LLVision INSTANCE = null;
 
@@ -36,10 +30,8 @@ public class LLVision extends SubsystemBase {
         limelight = RobotMap.getInstance().LIMELIGHT;
         this.telemetry = telemetry;
 //        telemetry = RobotCore.getTelemetry();
-        result = null;
-        colorResults = null;
 
-        setPipeline(0);
+        setPipeline(4);
         start();
     }
 
@@ -62,43 +54,15 @@ public class LLVision extends SubsystemBase {
 
     public void updateResults() {
         result = limelight.getLatestResult();
-        colorResults = result.getColorResults();
-//        if (result != null) {
-//            if (result.isValid()) {
-//                colorResults = result.getColorResults();
-//            }
-//            else colorResults = null;
-//        }
-//        else colorResults = null;
+        pythonOutput = result.getPythonOutput();
+    }
+
+    public void setColorRange(double[] range) {
+        limelight.updatePythonInputs(range);
     }
 
     public double getSampleAngle() {
-        if (colorResults.isEmpty()) return CollectorConstants.PIVOT_PASSTHROUGH_POS;
-        List<List<Double>> corners = colorResults.get(0).getTargetCorners();
-
-        Point corner0 = new Point(corners.get(0).get(0), corners.get(0).get(1), Point.CARTESIAN);
-        Point corner1 = new Point(corners.get(1).get(0), corners.get(1).get(1), Point.CARTESIAN);
-
-        double angle = Math.toDegrees(Math.atan2(corner1.getY() - corner0.getY(), corner1.getX() - corner0.getX()));
-
-        angle = (angle + 180) / 360.0;
-
-        return angle;
-    }
-
-    private int getFarthestPointIndex(List<Point> list) {
-        double max = 0;
-        int index = 0;
-
-        for (int i = 0; i < list.size(); i++) {
-            double radius = list.get(i).getR();
-            if (radius > max) {
-                max = radius;
-                index = i;
-            }
-        }
-
-        return index;
+        return pythonOutput[0];
     }
 
     @Override
@@ -106,13 +70,16 @@ public class LLVision extends SubsystemBase {
         updateResults();
 
         telemetry.addLine();
-        telemetry.addData("Number of color results", colorResults.size());
-        for (LLResultTypes.ColorResult cr : colorResults) {
-            telemetry.addData("Corner num", cr.getTargetCorners().size());
-            telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
-        }
+        telemetry.addData("same?", result.equals(lastResult));
         telemetry.addData("angle", getSampleAngle());
+        telemetry.addData("time", limelight.getTimeSinceLastUpdate());
+        telemetry.addData("Running?", limelight.isRunning());
+        telemetry.addData("Connected?", limelight.isConnected());
         telemetry.addData("tx", result.getTx());
         telemetry.addData("ty", result.getTy());
+        telemetry.addData("LL CPU", limelight.getStatus().getCpu());
+        telemetry.addData("Valid result", result.isValid());
+
+        lastResult = limelight.getLatestResult();
     }
 }
