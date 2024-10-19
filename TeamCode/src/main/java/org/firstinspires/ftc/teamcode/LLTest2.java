ba2d1275fe77ee7658bc3f521158f2ecababfe54
Nicolas <nicsilvc@gmail.com>
2024-10-19 16:24:57 -0700

    I did a stupid

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LLTest2.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.vision.LLVision;
+
+@TeleOp
+public class LLTest2 extends OpMode {
+    LLVision limelight;
+    Servo servo;
+
+    @Override
+    public void init() {
+        LLVision.resetInstance();
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        RobotMap.getInstance().init(hardwareMap);
+        limelight = LLVision.getInstance();
+        servo = RobotMap.getInstance().PIVOT;
+
+        limelight.setPipeline(4);
+        limelight.start();
+    }
+
+    @Override
+    public void loop() {
+        limelight.periodic();
+        servo.setPosition(limelight.getServoPos());
+
+        telemetry.addData("is running", limelight.isRunning());
+        telemetry.addData("Servo Angle", limelight.getServoPos());
+        telemetry.addData("Sample Angle", limelight.getSampleAngle());
+        telemetry.update();
+    }
+}
