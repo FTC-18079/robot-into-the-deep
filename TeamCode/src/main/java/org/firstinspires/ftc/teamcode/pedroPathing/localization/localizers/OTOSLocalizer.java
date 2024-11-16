4b6d344159a8a050d7299117b9a3436a3a440f3a
Nicolas <nicsilvc@gmail.com>
2024-11-15 16:08:14 -0800

    Retuned localization and mass (it got heavier :skull:)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/OTOSLocalizer.java
@@ -69,8 +69,8 @@ public class OTOSLocalizer extends Localizer {
 
         otos.setOffset(new SparkFunOTOS.Pose2D(0.3125,0.0,0.0));
 
-        otos.setLinearScalar(0.9998);
-        otos.setAngularScalar(0.9774);
+        otos.setLinearScalar(1.01548);
+        otos.setAngularScalar(0.97168);
 
         otos.calibrateImu();
         otos.resetTracking();
