df4a12ab189604f5bbca4347a2053afb4b37061f
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-12-04 15:56:09 -0800

    63 add axon encoder (#64)
    * Added encoder class and test
    
    * Removed useless readings from test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/opmode/AxonEncoderTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.util.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+import org.firstinspires.ftc.teamcode.util.hardware.AxonEncoder;
+
+@TeleOp
+public class AxonEncoderTest extends OpMode {
+    private AxonEncoder encoder;
+
+    @Override
+    public void init() {
+        encoder = new AxonEncoder(hardwareMap.get(AnalogInput.class, "pivotEncoder"));
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Encoder pos", encoder.getPosition());
+        telemetry.update();
+    }
+}
