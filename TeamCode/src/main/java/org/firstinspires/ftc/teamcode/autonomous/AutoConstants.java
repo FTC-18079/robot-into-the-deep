1d116a8916e1f37667c034d8057770bec538b7f2
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-12-15 01:16:23 -0800

    73 specimen auto (#92)
    * Started specimen auto sequence
    
    * Completed Specimen auto sequence
    
    * Finished up auto for today, tweak tomorrow
    
    ---------
    
    Co-authored-by: Anthony <@IDKProd>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoConstants.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.RobotStatus;
 public class AutoConstants {
     // Scoring poses
     public static double CHAMBER_X_POSITION = 31.25;
+    public static double CHAMBER_RIGHT_Y_POSITION = 72;
     public static double CHAMBER_RIGHT_Y_POSITION = 66;
 
     public static final Pose BASKET_SCORE_POSE = new Pose(17, 127, Math.toRadians(315));
@@ -17,7 +18,7 @@ public class AutoConstants {
     public static final Pose CHAMBER_RIGHT_SCORE_POSE = new Pose(CHAMBER_X_POSITION, CHAMBER_RIGHT_Y_POSITION, Math.toRadians(180));
 
     // Parking poses
-    public static final Pose OBVZONE_PARKING_POSE = new Pose(14, 38, Math.toRadians(0));
+    public static final Pose OBVZONE_PARKING_POSE = new Pose(14, 10, Math.toRadians(0));
     public static final Pose ASCENT_PARKING_POSE = new Pose(60, 98, Math.toRadians(90));
 
     // Alliance alliance poses
@@ -31,7 +32,7 @@ public class AutoConstants {
     public static final Pose RIGHT_NEUTRAL_SAMPLE = new Pose(46, 121.25);
 
     // Specimen collect poses
-    public static double WALL_COLLECT_X_POSITION = 20;
+    public static double WALL_COLLECT_X_POSITION = 20.5;
     public static double WALL_COLLECT_Y_POSITION = 35;
 
     public static Pose checkAlliance(Pose pose) {
