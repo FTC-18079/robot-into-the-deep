1e0f3a68b55b730a92ac27c03268ba258587da08
Nicolas <nicsilvc@gmail.com>
2024-12-15 12:21:04 -0800

    Auto tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoConstants.java
@@ -12,7 +12,7 @@ public class AutoConstants {
     public static double CHAMBER_X_POSITION = 31.25;
     public static double CHAMBER_RIGHT_Y_POSITION = 74;
 
-    public static final Pose BASKET_SCORE_POSE = new Pose(15, 125, Math.toRadians(315));
+    public static final Pose BASKET_SCORE_POSE = new Pose(14, 125, Math.toRadians(315));
     public static final Pose CHAMBER_LEFT_SCORE_POSE = new Pose(CHAMBER_X_POSITION, 80, Math.toRadians(180));
     public static final Pose CHAMBER_RIGHT_SCORE_POSE = new Pose(CHAMBER_X_POSITION, CHAMBER_RIGHT_Y_POSITION, Math.toRadians(180));
 
