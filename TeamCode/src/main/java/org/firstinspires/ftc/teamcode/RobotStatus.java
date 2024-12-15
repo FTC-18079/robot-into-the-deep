5aa36eed0207fbce475e248c8c573d3876eba2b6
Nicolas <130109338+niccsilv@users.noreply.github.com>
2024-12-14 16:25:46 -0800

    65 climb subsystem (#81)
    * Set up climb subsystem
    
    * Started Climb Sequence Command
    
    * Made climb sequence
    
    * Developed Climb Sequence
    
    * Added motor reset
    
    * More climb changes
    
    * Changed climb engagement
    
    * Climb wokred! (almost)
    
    * Changed climb timings
    
    * Made a fix to climb
    
    * CLIMBEDDDDgit add .!
    
    * Added climb hooks
    
    * Refactored and set up ascent three command
    
    * Disabled climb hooks
    
    * Finished climb
    
    * Climb tweaks
    
    * Ascent Two Command Done
    
    ---------
    
    Co-authored-by: Anthony <@IDKProd>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStatus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStatus.java
@@ -11,11 +11,16 @@ public class RobotStatus {
         DISABLED, AUTONOMOUS_INIT, AUTONOMOUS_ENABLED, TELEOP_INIT, TELEOP_ENABLED
     }
 
+    public enum ClimbState {
+        DISABLED, STARTED, ENGAGED, READY, CLIMBING, CLIMBED
+    }
+
     public static long delayMs = 0;
     public static Alliance alliance = Alliance.NONE;
     public static RobotState robotState = RobotState.DISABLED;
     public static boolean liveView = false;
     public static Pose robotPose = new Pose();
+    public static ClimbState climbState = ClimbState.DISABLED;
 
     public static void resetValues() {
         delayMs = 0;
@@ -23,6 +28,15 @@ public class RobotStatus {
         robotState = RobotState.DISABLED;
         liveView = false;
         robotPose = new Pose();
+        climbState = ClimbState.DISABLED;
+    }
+
+    public static void setClimbState(ClimbState state) {
+        climbState = state;
+    }
+
+    public static boolean isClimbReady() {
+        return climbState == ClimbState.READY;
     }
 
     public static boolean isEnabled() {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/climb/ClimbConstants.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.climb;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class ClimbConstants {
+    // PID
+
+    public static double kP = 1.0;
+    public static double kI = 0.0;
+    public static double kD = 0.0;
+    public static double kF = 0.2;
+
+    // Constants
+
+    public static double CLIMB_ERROR_TOLERANCE = 0.0;
+
+    // POSITIONS
+
+    public static double CLIMB_LATCH_POSITION = 700.0;
+    public static double CLIMB_UNSPOOLED_POSITION = -5950;
+    public static double CLIMB_PIVOT_POSITION = 800.0;
+    public static double CLIMB_IN_POSITION = 900;
+
+    public static double RIGHT_HOOK_READY_POSITION = 0.6;
+    public static double LEFT_HOOK_READY_POSITION = 0.4;
+
+    public static double RIGHT_HOOK_ENGAGE_POSITION = 0.0;
+    public static double LEFT_HOOK_ENGAGE_POSITION = 1.0;
+}
