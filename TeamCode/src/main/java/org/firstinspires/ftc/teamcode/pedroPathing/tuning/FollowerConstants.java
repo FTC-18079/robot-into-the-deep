bc0587c82b15e71154844617a65aaa2ecdae45e7
Nicolas <nicsilvc@gmail.com>
2025-01-10 19:08:48 -0800

    Retuned velocities and deceleration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
@@ -20,6 +20,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.util.KalmanFilterParameters;
  * @author Harrison Womack - 10158 Scott's Bots
  * @version 1.0, 3/4/2024
  */
+
 @Config
 public class FollowerConstants {
     // This section is for configuring your motors
@@ -35,8 +36,8 @@ public class FollowerConstants {
 
     // This section is for setting the actual drive vector for the front left wheel, if the robot
     // is facing a heading of 0 radians with the wheel centered at (0,0)
-    private static double xMovement = 69.64548;
-    private static double yMovement = 41.56312;
+    private static double xMovement = 76.205;
+    private static double yMovement = 51.4708;
     private static double[] convertToPolar = Point.cartesianToPolar(xMovement, -yMovement);
     public static Vector frontLeftVector = MathFunctions.normalizeVector(new Vector(convertToPolar[0], convertToPolar[1]));
 
@@ -96,11 +97,11 @@ public class FollowerConstants {
 
     // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
     // if not negative, then the robot thinks that its going to go faster under 0 power
-    public static double forwardZeroPowerAcceleration = -78.6853;
+    public static double forwardZeroPowerAcceleration = -47.601;
 
     // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
     // if not negative, then the robot thinks that its going to go faster under 0 power
-    public static double lateralZeroPowerAcceleration = -98.0606;
+    public static double lateralZeroPowerAcceleration = -114.94575;
 
     // A multiplier for the zero power acceleration to change the speed the robot decelerates at
     // the end of paths.
