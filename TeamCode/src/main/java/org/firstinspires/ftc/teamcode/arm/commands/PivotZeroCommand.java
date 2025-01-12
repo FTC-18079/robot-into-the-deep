300947d88aee9507b0fb959c3c7018e1b3ef920f
Nicolas <nicsilvc@gmail.com>
2025-01-11 19:07:58 -0800

    Fixed zeroing fr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/commands/PivotZeroCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/commands/PivotZeroCommand.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.arm.commands;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -7,9 +8,13 @@ import org.firstinspires.ftc.teamcode.arm.Arm;
 import org.firstinspires.ftc.teamcode.arm.ArmConstants;
 import org.firstinspires.ftc.teamcode.util.commands.Commands;
 
+@Config
 public class PivotZeroCommand extends CommandBase {
     private final Arm arm;
     private final ElapsedTime timer;
+
+    public static double COMMAND_TIMEOUT = 750;
+    public static double PIVOT_POWER = -0.10;
 
     public PivotZeroCommand() {
         arm = Arm.getInstance();
@@ -25,21 +30,21 @@ public class PivotZeroCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return timer.milliseconds() > ArmConstants.ZEROING_TIMEOUT;
+        return timer.milliseconds() > COMMAND_TIMEOUT;
     }
 
     @Override
     public void execute() {
-        arm.setPivotPower(-0.35);
+        arm.setPivotPower(PIVOT_POWER);
     }
 
     @Override
     public void end(boolean interrupted) {
         arm.setPivotPower(0);
         arm.resetPivotEncoder();
-        arm.setPivotPos(0);
 
         Commands.waitMillis(30)
+                .andThen(Commands.runOnce(() -> arm.setPivotPos(arm.getPivotPos())))
                 .andThen(Commands.runOnce(() -> arm.pivotZeroing = false))
                 .schedule();
     }
