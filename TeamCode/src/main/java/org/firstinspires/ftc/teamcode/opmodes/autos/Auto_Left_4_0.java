8de8ef4257e808f7e4312e8fb5fe39dfb57a570e
Nicolas <nicsilvc@gmail.com>
2025-01-12 16:46:59 -0800

    Pre-playoffs fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Auto_Left_4_0.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Auto_Left_4_0.java
@@ -39,7 +39,7 @@ public class Auto_Left_4_0 extends AutoTemplate {
     private final Pose scoreOnePose = AutoConstants.BASKET_SCORE_POSE.copy();
     private final Pose collectTwoPose = new Pose(18.5, 127.5, Math.toRadians(0));
     private final Pose scoreTwoPose = AutoConstants.BASKET_SCORE_POSE;
-    private final Pose collectThreePose = new Pose(20, 125.5, Math.toRadians(27));
+    private final Pose collectThreePose = new Pose(21.5, 125.5, Math.toRadians(27));
     private final Pose scoreThreePose = AutoConstants.BASKET_SCORE_POSE.copy();
 
     // Paths
