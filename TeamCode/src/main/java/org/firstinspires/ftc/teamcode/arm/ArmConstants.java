8c3959c82da7510bb1f91c632064b696e2fe906c
Nicolas <nicsilvc@gmail.com>
2025-02-01 09:30:22 -0800

    Tuned odometry and arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/ArmConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm/ArmConstants.java
@@ -10,7 +10,7 @@ public class ArmConstants {
      */
 
     // PID
-    public static double SLIDE_kP = 0.025;
+    public static double SLIDE_kP = 0.023;
     public static double SLIDE_kI = 0.0;
     public static double SLIDE_kD = 0.0003;
     public static double SLIDE_kF = 0.1;
